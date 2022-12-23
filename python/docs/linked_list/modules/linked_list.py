7f69afb42310a784ef6d71d91fc82798ed10ad02
Harper Ellis Foley <109574453+hfoley2013@users.noreply.github.com>
2022-12-23 14:54:29 -0800

    Delete python/docs/linked_list directory

--- python/docs/linked_list/modules/linked_list.py
+++
@@ -1,6 +0,0 @@
-# Create a Node class that has properties of the value stored in the Node and a pointer to the next Node
-
-class Node:
-  def __init__(self, value, next=None):
-    self.value = value
-    self.next = next
