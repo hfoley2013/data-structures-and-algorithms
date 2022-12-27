2f723dfcebbe0d198decc6e62289a208a5758034
Harper Foley <harper.e.foley@gmail.com>
2022-12-27 11:39:37 -0800

    Moves code challenge 5 over from docs to data_structures folder. NOTE: Tests designed for code challenge 5 inside docs folder were custom and do not appear to reflect the desired outputs from the provided tests in the data_strucutres/tests folder. The provided tests for the .insert method appear to want a string of the inserted values, however the instructions for code challenge 5 define this as a separate method. In the existing code base, this is the to_string method, so named in accordance with the code challenge 5 instructions. This method needs to be called in order to return a string representation of the linked list

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,16 +1,49 @@
+# Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node
+
+class Node:
+  """
+  Instantiates a class Node for use in Linked Lists.
+  """
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+# Create a LinkedList class
 class LinkedList:
+  """
+  Instantiates a Linked List class to which Nodes can be inserted, found, and their link chain returned as a string.
+  """
+  def __init__(self):
+    self.head = None
+
+  def insert(self, value):
     """
-    Put docstring here
+    Adds a new node with that value to the head of the list with an O(1) Time performance.
     """
+    new_node = Node(value, self.head)
+    self.head = new_node
 
-    def __init__(self):
-        # initialization here
-        pass
+  def includes(self, value):
+    """
+    Indicates whether that value exists as a Node’s value somewhere within the list.
+    """
+    node = self.head
+    while node is not None:
+        if node.value == value:
+            return True
+        node = node.next
+    return False
 
-    def some_method(self):
-        # method body here
-        pass
-
-
-class TargetError:
-    pass
+  def to_string(self):
+    """
+    Returns a string representing all the values in the Linked List, formatted as: "{ a } -> { b } -> { c } -> NULL"
+    """
+    values = []
+    node = self.head
+    while node is not None:
+        values.append(str(node.value))
+        node = node.next
+    if len(values) == 0:
+        return "NULL"
+    return " -> ".join(values) + " -> NULL"
--- python/tests/data_structures/test_linked_list.py
+++ python/tests/data_structures/test_linked_list.py
@@ -1,23 +1,23 @@
 import pytest
-from data_structures.linked_list import LinkedList
+from data_structures.linked_list import LinkedList, Node
 
 
 def test_exists():
     assert LinkedList
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_instantiate():
     assert LinkedList()
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_empty_head():
     linked = LinkedList()
     assert linked.head is None
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_populated_head():
     linked = LinkedList()
     linked.insert("apple")
@@ -53,7 +53,7 @@ def test_to_string_double():
     assert str(linked_list) == "{ banana } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_includes_true():
     linked_list = LinkedList()
 
@@ -64,7 +64,7 @@ def test_includes_true():
     assert linked_list.includes("apple")
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_includes_false():
     linked_list = LinkedList()
 
@@ -73,3 +73,22 @@ def test_includes_false():
     linked_list.insert("banana")
 
     assert not linked_list.includes("cucumber")
+
+def test_to_string():
+    linked_list = LinkedList()
+    linked_list.insert(1)
+    linked_list.insert(2)
+    linked_list.insert(3)
+
+    assert linked_list.to_string() == "3 -> 2 -> 1 -> NULL"
+
+def test_to_string_one_node():
+    linked_list = LinkedList()
+    linked_list.insert(1)
+
+    assert linked_list.to_string() == "1 -> NULL"
+
+def test_to_string_no_nodes():
+    linked_list = LinkedList()
+
+    assert linked_list.to_string() == "NULL"
