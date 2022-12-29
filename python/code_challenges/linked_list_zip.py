3966a8a8e45d7a610761c543b6312e0171afe40e
Harper Foley <harper.e.foley@gmail.com>
2022-12-29 13:50:42 -0800

    Creates function zip_lists that combines two linked lists into one list with alternating values.  - [ ] Top-level README “Table of Contents” is updated - [ ] README for this challenge is complete        - [ ] Summary, Description, Approach & Efficiency, Solution        - [ ] Picture of whiteboard        - [ ] Link to code  - [ ] Feature tasks for this challenge are completed  - [ ] Unit tests written and passing        - [ ] “Happy Path” - Expected outcome        - [ ] Expected failure        - [ ] Edge Case (if applicable/obvious)

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,40 @@
-def zip_lists(a, b):
-    pass
+from data_structures.linked_list import *
+
+def zip_lists(list1, list2):
+    """
+    Takes two linked lists as arguments and returns a single list with the alternating values from the two lists in the order they appear in their original lists.
+
+    Example:
+    list1 = {1} -> {3} -> {2} -> null
+    list2 = {5} -> {9} -> {4} -> null
+
+    Expected Output: {1} -> {5} -> {3} -> {9} -> {2} -> {4} -> null
+    """
+
+    current1 = list1.head
+    current2 = list2.head
+
+    if current1 is None:
+        return list2
+
+    if current2 is None:
+        return list1
+
+    while current1 and current2:
+        # store next values
+        current1_next = current1.next
+        current2_next = current2.next
+
+        # insert second value after first value
+        list1.insert_after(current1.value, current2.value)
+
+        # move the pointers forward one
+        current1 = current1_next
+        current2 = current2_next
+
+    # account for list2 being longer than list1
+    while current2:
+        list1.append(current2.value)
+        current2 = current2.next
+    
+    return list1
