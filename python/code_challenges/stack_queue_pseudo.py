ba2915afeb1ba4f3f1ba165b3617e3a892716ecf
Harper Foley <harper.e.foley@gmail.com>
2023-01-02 15:04:14 -0800

    Creats PseudoQueue class and adds functionaility to pass all provided tests.

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,30 @@
 from data_structures.stack import Stack
-
+from data_structures.invalid_operation_error import InvalidOperationError
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack_1 = Stack()
+        self.stack_2 = Stack()
+
+    def enqueue(self, value):
+        if self.stack_1.is_empty() is False:
+            while self.stack_1.is_empty() is False:
+                node = self.stack_1.pop()
+                self.stack_2.push(node)
+        self.stack_2.push(value)
+
+
+    def dequeue(self):
+        if self.stack_2.is_empty() is False:
+            while self.stack_2.is_empty() is False:
+                node = self.stack_2.pop()
+                self.stack_1.push(node)
+        result = self.stack_1.pop()
+        return result
+
+
+
+    # def is_empty(self):
+    #     if self.top:
+    #         return False
+    #     return True
