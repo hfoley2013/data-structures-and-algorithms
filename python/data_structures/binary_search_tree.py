6c766e1e0525a14eff0a4ccb4b24b0ebf7f5ceae
Harper Foley <harper.e.foley@gmail.com>
2023-01-06 10:27:41 -0800

    Adds BinaryTree and BinarySearchTree classes that pass all tests.

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,33 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
-    """
-    Put docstring here
-    """
+    def add(self, value):
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+            return
 
-    def __init__(self):
-        # initialization here
-        pass
+        node = self.root
+        while True:
+            if value < node.value:
+                if not node.left:
+                    node.left = new_node
+                    return
+                node = node.left
+            else:
+                if not node.right:
+                    node.right = new_node
+                    return
+                node = node.right
 
-    def some_method(self):
-        # method body here
-        pass
+    def contains(self, value):
+        node = self.root
+        while node:
+            if value == node.value:
+                return True
+            elif value < node.value:
+                node = node.left
+            else:
+                node = node.right
+        return False
--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -3,14 +3,62 @@ class BinaryTree:
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, root=None):
+        self.root = root
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+        values = []
+        stack = [self.root]
+        while stack:
+            node = stack.pop()
+            values.append(node.value)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return values
+
+    def in_order(self):
+        values = []
+        stack = []
+        node = self.root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            values.append(node.value)
+            node = node.right
+        return values
+
+    def breadth_first(self):
+        values = []
+        queue = [self.root]
+        while queue:
+            node = queue.pop(0)
+            values.append(node.value)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return values
+
+
+    def post_order(self):
+        values = []
+        stack = [self.root]
+        while stack:
+            node = stack.pop()
+            values.append(node.value)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return values[::-1]
 
 
 class Node:
-    pass
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
